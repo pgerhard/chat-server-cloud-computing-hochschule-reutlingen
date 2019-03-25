fa8bd31ca4f7376c403db835da441fd451228f26
Philipp Gerhard <philipp.gerhard@me.com>
2019-03-25 17:26:45 +0100

    Moved routing from app module to app routing module

--- chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app-routing.module.ts
+++ chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { ChatComponent } from "./components/chat/chat.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "chatroom", component: ChatComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app.module.ts
+++ chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app.module.ts
@@ -1,37 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ChatService } from './chat.service';
-import { ChatMessageComponent } from './chat-message/chat-message.component';
-
-const appRoutes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'chatroom', component: ChatComponent}
-];
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { ChatComponent } from "./components/chat/chat.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ChatService } from "./chat.service";
+import { ChatMessageComponent } from "./chat-message/chat-message.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ChatComponent,
-    ChatMessageComponent,
-
+    ChatMessageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
-    HttpClientModule,
-    FormsModule,
-
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [ChatService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
