46be934be7b7ce972c39c8336d75e52f38919bbc
markovaa <Arina.Markova@student.reutlingen-university.de>
2019-03-25 15:56:41 +0100

    chat und login Komponente  implementiert

--- chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app.component.ts
+++ chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{ChatService} from "./chat.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chat-server-cloud-computing-hochschule-reutlingen-frontend';
+
+  constructor(chatService: ChatService){}
 }
+
--- chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app.module.ts
+++ chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ChatService } from './chat.service';
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'chatroom', component: ChatComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChatComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
+
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
