17d40275d52e2087b52998d96af0efb7a7121738
Philipp Gerhard <philipp.gerhard@me.com>
2019-03-29 00:27:45 +0100

    Minor backend change to allow changing listening port via process env

--- chat-server-cloud-computing-hochschule-reutlingen-backend/src/index.js
+++ chat-server-cloud-computing-hochschule-reutlingen-backend/src/index.js
@@ -23,6 +23,7 @@ io.on("connection", function(socket) {
   });
 });
 
-http.listen(port, function() {
-  console.log("listening on *:" + port);
+http.listen(process.env.PORT || 3000, function() {
+  const port = process.env.PORT ? process.env.PORT : 3000;
+  console.log(`listening on ${port}`);
 });
