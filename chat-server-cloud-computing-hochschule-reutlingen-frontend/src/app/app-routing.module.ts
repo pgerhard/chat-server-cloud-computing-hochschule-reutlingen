af83d8313a2a73113591ef53a798d18b5a3b98b2
Philipp Gerhard <philipp.gerhard@me.com>
2019-03-28 11:34:13 +0100

    Changed routing to redirect to login from root and added route to login

--- chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app-routing.module.ts
+++ chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { LoginComponent } from "./login/login.component";
 import { ChatComponent } from "./chat/chat.component";
 
 const appRoutes: Routes = [
-  { path: "", component: LoginComponent },
-  { path: "chatroom", component: ChatComponent }
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "chatroom", component: ChatComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
