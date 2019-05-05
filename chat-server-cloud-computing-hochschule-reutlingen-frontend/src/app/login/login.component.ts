c5d4e59f5edf30a31aaf1f288ca762b8b72616e6
markovaa <Arina.Markova@student.reutlingen-university.de>
2019-05-05 23:36:06 +0200

    secure password and login input , info window for inputs

--- chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/login/login.component.ts
+++ chat-server-cloud-computing-hochschule-reutlingen-frontend/src/app/login/login.component.ts
@@ -11,7 +11,15 @@ import { UserService } from "../user.service";
 export class LoginComponent implements OnInit {
   constructor(private router: Router, private userService: UserService) {}
 
-  ngOnInit() {}
+  checkLetter = false;
+  checkCapital = false;
+  checkNumber = false;
+  checkLength = false;
+
+  ngOnInit() {
+    this.nameInput();
+    this.passwordInput();
+  }
 
   btnClick() {
     this.router.navigateByUrl("/chatroom");
@@ -19,14 +27,127 @@ export class LoginComponent implements OnInit {
 
   loginUser(e) {
     e.preventDefault();
-    var username = e.target.elements[0].value;
-    if (username.length < 1 || username.length > 20) {
-      console.log("Bitte gültige Eingabe");
-    } else {
-      const user = new User();
-      user.name = username;
-      this.userService.login(user);
-      this.btnClick();
+
+
+        console.log("!!!" + e.target.elements[0].value);
+        console.log(e.target.elements[1].value);
+
+        var username = e.target.elements[0].value;
+
+
+          const user = new User();
+          user.name = username;
+          this.userService.login(user);
+
+          this.btnClick();
+
+
+  }
+
+
+  checkValidPasword(){
+    if(this.checkCapital && this.checkLength && this.checkLetter && this.checkNumber){
+      console.log("!!! " + this.checkNumber + " " + this.checkLength )
+      return true;
+    }
+    else{
+      return false;
     }
   }
+
+  nameInput(){
+    var username = document.getElementById("nam");
+    var nameinput = document.getElementById("nameinp");
+
+    // When the user clicks on the name field, show the message box
+    username.onfocus = function() {
+      document.getElementById("nameinput").style.display = "block";
+    }
+
+    // When the user clicks outside of the name field, hide the message box
+    username.onblur = function() {
+      document.getElementById("nameinput").style.display = "none";
+    }
+
+    username.onkeyup=function () {
+      var name_Let = /[a-zA-Z]/g;
+      if(username.value.match(name_Let) && (username.value.length>2 && username.value.length<10)) {
+        nameinput.classList.remove("invalid");
+        nameinput.classList.add("valid");
+        return true;
+      } else {
+        nameinput.classList.remove("valid");
+        nameinput.classList.add("invalid");
+      }
+    }
+  }
+
+  passwordInput(){
+
+    var myInput = document.getElementById("psw");
+    var letter = document.getElementById("letter");
+    var capital = document.getElementById("capital");
+    var number = document.getElementById("number");
+    var length = document.getElementById("length");
+
+
+// When the user clicks on the password field, show the message box
+    myInput.onfocus = function() {
+      document.getElementById("message").style.display = "block";
+    }
+
+// When the user clicks outside of the password field, hide the message box
+    myInput.onblur = function() {
+      document.getElementById("message").style.display = "none";
+    }
+
+// When the user starts to type something inside the password field
+    myInput.onkeyup = function() {
+      //  lowercase letters
+      var lowerCaseLetters = /[a-z]/g;
+      if(myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+        this.checkLetter=true;
+      } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+      }
+
+      // capital letters
+      var upperCaseLetters = /[A-Z]/g;
+      if(myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+        this.checkCapital = true;
+      } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+      }
+
+      // numbers
+      var numbers = /[0-9]/g;
+      if(myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+        this.checkNumber=true;
+      } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+      }
+
+      // length
+      if(myInput.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+        this.checkLength = true;
+      } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+      }
+
+    }
+    return this.checkValidPasword();
+  }
+
 }
